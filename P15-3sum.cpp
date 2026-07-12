515c4c75e94a48bd6c7bfd90150d1b550f76e30e
Subharthi Dutta <133211864+subhaiml@users.noreply.github.com>
2026-07-12 18:21:37 +0530

    Refactor threeSum function for clarity and efficiency

--- P15-3sum.cpp
+++ P15-3sum.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end()); 
-        std::vector<std::vector<int>> result;
-        int n = nums.size();
-        
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+    int st=0,l=0,r=0;
+    vector<vector<int>> ans;
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<nums.size()-2;i++){
+        if(i>0 && nums[i]==nums[i-1]){
+            continue;
+        }
+        int l=i+1;
+        int r=nums.size()-1;
+
+        while(l<r){
+            int sum=nums[i]+nums[l]+nums[r];
+            if(sum==0){
+                ans.push_back({nums[i],nums[l],nums[r]});
+                l++;
+                r--;
+                while(l<r && nums[l]==nums[l-1]) l++;
+                while(l<r && nums[r]==nums[r+1]) r--;
             }
-            int left = i + 1;
-            int right = n - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    result.push_back({nums[i], nums[left], nums[right]});
-                    left++;
-                    right--;
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
-                }
+            else if(sum<0){
+                l++;
+            }
+            else{
+                r--;
             }
         }
-        return result;
-        
     }
+    return ans;
+}
 };
