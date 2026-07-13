4230166efe6ebaeb30c795c310cdcb2eec1f3ce5
Subharthi Dutta <133211864+subhaiml@users.noreply.github.com>
2026-07-13 22:54:56 +0530

    Fix formatting issue in P15-3sum.cpp

--- P15-3sum.cpp
+++ P15-3sum.cpp
@@ -6,7 +6,7 @@ public:
     sort(nums.begin(),nums.end());
     for(int i=0;i<nums.size()-2;i++){
         if(i>0 && nums[i]==nums[i-1]){
-            continue;
+            continue ;
         }
         int l=i+1;
         int r=nums.size()-1;
